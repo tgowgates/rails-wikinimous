ba4c393a07a876120c8c0ce1fe09a1afe1c5b35b
Tom Gow-Gates <tomrgg@gmail.com>
2018-07-31 13:21:54 +0100

    added articles db and seeded faker data

---
+++ app/models/article.rb
@@ -0,0 +1,25 @@
+class Article < ApplicationRecord
+
+  def index
+    @articles = Article.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |index|
+  Article.create(
+    title: Faker::Ancient.god,
+    content: Faker::ChuckNorris.fact )
+end
