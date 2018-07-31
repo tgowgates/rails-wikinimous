f913da30786da13e4c532221f4c113b4e78895c1
Tom Gow-Gates <tomrgg@gmail.com>
2018-07-31 14:21:20 +0100

    completed CRUD but no styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,26 +1,3 @@
 class Article < ApplicationRecord
-
-  def index
-    @articles = Article.all
-  end
-
-  def create
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def show
-    @articles = Article.find(params(:id))
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
+  validates :title, presence: true
 end
